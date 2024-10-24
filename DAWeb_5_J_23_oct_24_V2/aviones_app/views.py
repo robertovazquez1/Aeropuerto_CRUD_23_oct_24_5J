3bace746b5d0a7146807690b3d014ac50c7024c5
Roberto Vazquez <a.22308051281326@cbtis128.edu.mx>
2024-10-24 12:03:24 -0600

    Add files via upload

---
+++ DAWeb_5_J_23_oct_24_V2/aviones_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Aviones
+# Create your views here.
+
+def listadoAviones(request):
+    losaviones=Aviones.objects.all()
+    return render(request,"gestionarAviones.html",{"Mis Aviones":losaviones})
